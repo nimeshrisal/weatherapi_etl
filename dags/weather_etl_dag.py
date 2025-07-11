cd70de9bb432de4d55889478fd892fb6e4cc244c
Nimesh Risal <risal.nms@gmail.com>
2025-07-11 08:42:47 +0545

    python operator

--- dags/weather_etl_dag.py
+++ dags/weather_etl_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator  
+from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import os
 import sys
@@ -16,20 +16,20 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG(
+with DAG(
     dag_id='weather_etl_pipeline',
     default_args=default_args,  
     description='A simple ETL pipeline for weather data that runs every 5 minutes',
-    schedule_interval='*/5 * * * *',  # Every 5 minutes
+    schedule ='*/5 * * * *',  # Every 5 minutes
     start_date=datetime(2023, 10, 1),  # Start date for the DAG
     catchup=False,  # Do not backfill
     tags=['weather', 'etl', 'pipeline']  # Tags for better organization in Airflow UI
-)
+) as dag:
 
-run_etl = PythonOperator(
-    task_id='run_etl',
-    python_callable=lambda: __import__('main').main(),  # Import main.py and call main function
-    dag=dag,
-)
+    run_etl = PythonOperator(
+        task_id='run_etl',
+        python_callable=lambda: __import__('main').main(),  # Import main.py and call main function
+        dag=dag,
+    )
 
-run_etl
+    run_etl
