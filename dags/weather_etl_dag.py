8db9d066df68624d2e6146499675fdd99f106aaa
Nimesh Risal <risal.nms@gmail.com>
2025-07-11 09:33:54 +0545

    python operator

--- dags/weather_etl_dag.py
+++ dags/weather_etl_dag.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import os
 import sys
 
+# Add parent directory to sys.path for main.py
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 default_args = {
@@ -18,18 +19,16 @@ default_args = {
 
 with DAG(
     dag_id='weather_etl_pipeline',
-    default_args=default_args,  
+    default_args=default_args,
     description='A simple ETL pipeline for weather data that runs every 5 minutes',
-    schedule ='*/5 * * * *',  # Every 5 minutes
-    start_date=datetime(2023, 10, 1),  # Start date for the DAG
-    catchup=False,  # Do not backfill
-    tags=['weather', 'etl', 'pipeline']  # Tags for better organization in Airflow UI
+    schedule_interval='*/5 * * * *',  # Corrected parameter
+    start_date=datetime(2023, 10, 1),
+    catchup=False,
+    tags=['weather', 'etl', 'pipeline'],
 ) as dag:
-
     run_etl = PythonOperator(
         task_id='run_etl',
         python_callable=lambda: __import__('main').main(),  # Import main.py and call main function
-        dag=dag,
     )
 
     run_etl
